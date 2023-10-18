f6299245347f7b7d09605666ac1499301b3f6f7a
Chirukaru36 <kpoojashree470@gmail.com>
2023-10-18 20:10:50 +0530

    Add project files.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringLengthLibrary;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringLength L = new StringLength();
+            string inputString = "hello, world";
+            int length = L.CalculateStringLength(inputString);
+            Console.WriteLine($"the length of the string is : {length}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringLengthLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLengthLibrary
+{
+    public delegate int FindLength(string s1);
+    public class StringLength
+
+    {
+        public int CalculateStringLength(string s1)
+        {
+            if(s1 == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return s1.Length;
+            }
+        }
+    }
+}
